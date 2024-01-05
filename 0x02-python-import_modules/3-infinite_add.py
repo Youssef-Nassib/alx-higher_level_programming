e47f69863d92d12b109b36e7f7a009da2441c52f
root <wissal2015.nassib@gmail.com>
2024-01-04 16:08:16 -0800

    l

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     thecount = len(sys.argv) - 1
     if thecount == 0:
         print("0 arguments.")
-    elif the count == 1:
+    elif thecount == 1:
         print("1 argument.")
     else:
         print("{} arguments:".format(thecount)
     for i in range(count):
-    print("{}: {}".format(i + 1,sys.argv[i + 1]))
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))

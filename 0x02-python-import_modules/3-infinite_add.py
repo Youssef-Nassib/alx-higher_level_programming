c604406436de29a76f09a8919dde2278bce6974f
root <wissal2015.nassib@gmail.com>
2024-01-04 16:02:44 -0800

    l

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    thecount = len(sys.argv) - 1
+    if thecount == 0:
+        print("0 arguments.")
+    elif the count == 1:
+        print("1 argument.")
+    else:
+        print("{} arguments:".format(thecount)
+    for i in range(count):
+    print("{}: {}".format(i + 1,sys.argv[i + 1]))

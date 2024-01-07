6d6e13d66d5804509a105d5ca0ae7efa492017e6
root <wissal2015.nassib@gmail.com>
2024-01-07 10:22:01 -0800

    l

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    newString = ""
-    for char in my_string:
-        if element != "c" and element != "C":
-            newString += elements
-        return newString
+    new_string = ""
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            new_string += elements
+            return new_string

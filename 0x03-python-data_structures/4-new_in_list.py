d09bf66f7ab1a5e793153488296ca8b6d9e2eb70
root <wissal2015.nassib@gmail.com>
2024-01-07 09:54:00 -0800

    l

--- 0x03-python-data_structures/4-new_in_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < o:
-       return my_list
-    if idx > len(my_list):
-        return my_list
-    new_list = list(my_list)
-    new_list[idx] = element
-    return new_list

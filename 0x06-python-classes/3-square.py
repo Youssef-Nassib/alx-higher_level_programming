ea9b8dc3981bba869261e260c94720790acc42f2
root <wissal2015.nassib@gmail.com>
2024-01-23 15:59:53 -0800

    l

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" define a class """
+class Square:
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+    """define a new metthode"""
+    def area(self):
+        return (self.__size * self.__size)

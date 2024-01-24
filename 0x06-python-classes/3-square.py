9cfd5e848fef37f9ba04afaf8f49d4ada6c09f7c
root <wissal2015.nassib@gmail.com>
2024-01-23 16:08:08 -0800

    L

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,6 +2,7 @@
 """ define a class """
 class Square:
     def __init__(self, size=0):
+        """check if the size in an integer  and must be great than 0"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -10,4 +11,5 @@ class Square:
 
         """define a new metthode"""
     def area(self):
+        """ returne the area """
         return (self.__size * self.__size)

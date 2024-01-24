50324c14462514f246103ffa5e02b9dd0300e673
root <wissal2015.nassib@gmail.com>
2024-01-23 16:17:40 -0800

    l

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-""" define a class """
+
+"""efine a class Square."""
+
 class Square:
+    """ methode  a square."""
+
     def __init__(self, size=0):
-        """check if the size in an integer  and must be great than 0"""
-        if type(size) != int:
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise TypeError("size must be >= 0")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
-        """define a new metthode"""
     def area(self):
-        """ returne the area """
+        """Return the current area of thsquare."""
         return (self.__size * self.__size)

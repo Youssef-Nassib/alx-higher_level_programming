10e490f32cc79a7bc8779d445721b40b93b1e582
root <wissal2015.nassib@gmail.com>
2024-04-08 16:28:38 -0700

    JS

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 var args = process.argv;
 var Narguments = args.length - 2;
 if (length(Narguments) == 0){
-	console.log('no argument');
+	console.log('No argument');
 }else {
-	cosole.log('Argument found');
+	cosole.log('Arguments found');
 }

3efa0a735335bba3c6fe41804476b172d51706dd
root <wissal2015.nassib@gmail.com>
2024-04-08 15:18:21 -0700

    JS

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/bin/node
-console.log('"C is fun"');
-console.log('“Python is cool”');
-console.log('“JavaScript is amazing”');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');

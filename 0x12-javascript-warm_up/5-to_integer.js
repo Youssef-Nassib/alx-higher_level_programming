01618d1eae45166344c64345788cdef499f0da9e
root <wissal2015.nassib@gmail.com>
2024-04-08 18:32:04 -0700

    JS

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const args = process.argv
-args = parseInt(args);
-if(isNaN(args)){
-	console.log('Not a number');}
-console.log('My number: ' + args);
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv[2]));
+}

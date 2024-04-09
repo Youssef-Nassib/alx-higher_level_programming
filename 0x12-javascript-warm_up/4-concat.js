48107348107451da8a9a3837d8f59a7d04f3db38
root <wissal2015.nassib@gmail.com>
2024-04-08 18:15:39 -0700

    JS

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined && process.argv[3] === undefined) 
-{
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-	if (process.argv[3] === undefined)
-	{
-		process.argv[3] = 'undefined';
-	}
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);

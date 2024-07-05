52663eaa70055e6c2b1f44e7bf3efc8d937d1c59
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-07-04 17:38:51 -0700

    l

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script to make a request to 0.0.0.0:5000/catch_me and capture "You got me!" response
-
-# Send a PUT request with custom header and body
-curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me

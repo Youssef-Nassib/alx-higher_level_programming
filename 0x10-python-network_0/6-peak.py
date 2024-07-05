429f9280d6c420c0e6bf7ac24130eef5f8a52029
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-07-04 17:31:49 -0700

    l

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,21 @@
-#!/usr/bin/python3
-
-
 def find_peak(list_of_integers):
-
-    if list_of_integers is None or len(list_of_integers) == 0:
+    """
+    Function to find a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
         return None
-
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-
-    mid_idx = int(len(list_of_integers) / 2)
-
-    if mid_idx != len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
-           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-    else:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
+    
+    def recursive_find_peak(nums, left, right):
+        if left == right:
+            return nums[left]
+        
+        mid = (left + right) // 2
+        
+        if nums[mid] < nums[mid + 1]:
+            return recursive_find_peak(nums, mid + 1, right)
+        elif nums[mid] < nums[mid - 1]:
+            return recursive_find_peak(nums, left, mid - 1)
         else:
-            return list_of_integers[mid_idx - 1]
-
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        a_list = list_of_integers[0:mid_idx]
-    else:
-        a_list = list_of_integers[mid_idx + 1:]
-
-    return find_peak(a_list)
+            return nums[mid]
+    
+    return recursive_find_peak(list_of_integers, 0, len(list_of_integers) - 1)

e279602f600f3aa57319d57ec652d4a0a94caf91
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-07-17 08:57:55 -0700

    find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,26 @@
+#!/usr/bin/python3
+""" a function that finds a peak in a list of unsorted integers """
+
+
 def find_peak(list_of_integers):
-    """
-    Function to find a peak in a list of unsorted integers.
-    """
-    if not list_of_integers:
+    """ find the peak number with the shortest algorithm"""
+    if list_of_integers == []:
         return None
-    
-    def recursive_find_peak(nums, left, right):
-        if left == right:
-            return nums[left]
-        
-        mid = (left + right) // 2
-        
-        if nums[mid] < nums[mid + 1]:
-            return recursive_find_peak(nums, mid + 1, right)
-        elif nums[mid] < nums[mid - 1]:
-            return recursive_find_peak(nums, left, mid - 1)
-        else:
-            return nums[mid]
-    
-    return recursive_find_peak(list_of_integers, 0, len(list_of_integers) - 1)
+
+    size = len(list_of_integers)
+    if size == 0:
+        return (None)
+    elif size == 1:
+        return (list_of_integers[0])
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size/2)
+    peak = list_of_integers[mid]
+    mylist = list_of_integers
+    if peak > mylist[mid - 1] and peak > mylist[mid + 1]:
+        return peak
+    elif peak < mylist[mid - 1]:
+        return find_peak(mylist[:mid])
+    else:
+        return find_peak(mylist[mid + 1:])
